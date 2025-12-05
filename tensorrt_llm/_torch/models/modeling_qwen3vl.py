fix: use trtllm LayerNorm to preserve numerical precision
Signed-off-by: Nekofish-L <liuxiangyang@mail.ustc.edu.cn>

tensorrt_llm/_torch/models/modeling_qwen3vl.py
@@ -32,6 +32,7 @@ from ...sampling_params import SamplingParams
 from ..attention_backend import AttentionMetadata
 from ..attention_backend.interface import PositionalEmbeddingParams, RopeParams
 from ..attention_backend.utils import get_attention_backend
+from ..modules.layer_norm import LayerNorm
 from ..modules.linear import Linear, TensorParallelMode
 from ..modules.mlp import MLP
 from ..modules.rotary_embedding import MRotaryEmbedding
@@ -411,8 +412,16 @@ class Qwen3VLVisionBlock(torch.nn.Module):
     def __init__(self, model_config: ModelConfig[PretrainedConfig], layer_idx: Optional[int]):
         super().__init__()
         config = model_config.pretrained_config.vision_config
-        self.norm1 = nn.LayerNorm(config.hidden_size, eps=1e-6)
-        self.norm2 = nn.LayerNorm(config.hidden_size, eps=1e-6)
+        self.norm1 = LayerNorm(
+            hidden_size=config.hidden_size,
+            eps=model_config.pretrained_config.text_config.rms_norm_eps,
+            dtype=model_config.pretrained_config.text_config.dtype,
+        )
+        self.norm2 = LayerNorm(
+            hidden_size=config.hidden_size,
+            eps=model_config.pretrained_config.text_config.rms_norm_eps,
+            dtype=model_config.pretrained_config.text_config.dtype,
+        )
         self.attn = Qwen3VLVisionAttention(model_config, layer_idx)
         self.mlp = Qwen3VLVisionMLP(model_config, layer_idx)
 
@@ -447,13 +456,16 @@ class Qwen3VLPatchMerger(torch.nn.Module):
         config = model_config.pretrained_config.vision_config
         self.hidden_size = config.hidden_size * (config.spatial_merge_size**2)
         self.use_postshuffle_norm = use_postshuffle_norm
-        self.norm = nn.LayerNorm(
-            self.hidden_size if use_postshuffle_norm else config.hidden_size, eps=1e-6
+        self.norm = LayerNorm(
+            hidden_size=self.hidden_size if use_postshuffle_norm else config.hidden_size,
+            eps=model_config.pretrained_config.text_config.rms_norm_eps,
+            dtype=model_config.pretrained_config.text_config.dtype,
         )
         self.linear_fc1 = Linear(
             in_features=self.hidden_size,
             out_features=self.hidden_size,
             bias=True,
+            dtype=model_config.pretrained_config.text_config.dtype,
             mapping=model_config.mapping,
             tensor_parallel_mode=TensorParallelMode.COLUMN,
             allreduce_strategy=model_config.allreduce_strategy,
@@ -463,6 +475,7 @@ class Qwen3VLPatchMerger(torch.nn.Module):
             in_features=self.hidden_size,
             out_features=config.out_hidden_size,
             bias=True,
+            dtype=model_config.pretrained_config.text_config.dtype,
             mapping=model_config.mapping,
             tensor_parallel_mode=TensorParallelMode.ROW,
             allreduce_strategy=model_config.allreduce_strategy,
@@ -492,7 +505,7 @@ class Qwen3VisionModel(torch.nn.Module):
 
         self.patch_embed = HFQwen3VLVisionPatchEmbed(
             config=self.config,
-        )
+        ).to(self.model_config.pretrained_config.text_config.dtype)
 
         self.pos_embed = nn.Embedding(self.config.num_position_embeddings, self.config.hidden_size)
         self.num_grid_per_side = int(self.config.num_position_embeddings**0.5)
@@ -685,7 +698,7 @@ class Qwen3VisionModelBase(nn.Module):
         self.model_config.quant_config = QuantConfig(
             kv_cache_quant_algo=self.model_config.quant_config.kv_cache_quant_algo
         )
-        self.visual = model_class(self.model_config).to(self.model_dtype)
+        self.visual = model_class(self.model_config)
 
         self.post_config()
 